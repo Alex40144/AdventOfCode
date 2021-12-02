0334cab5d0679fae4ad62d8284afad690848586b
AlexPegg <alexpeggsailing@gmail.com>
2021-12-02 19:44:38 +0000

    day 2

---
+++ day2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int forward = 0;
+int depth = 0;
+char line[20];
+
+int strlen(char str[]) {
+    int i = 0;
+    for (i = 0; str[i] != '\0'; ++i);
+    return i -1;
+}
+
+int main(void) {
+    FILE *fp;
+
+    fp=fopen("day2.txt", "r");
+
+    while(fgets(line, 20, fp)) {
+        int value = (int)line[strlen(line)-1] -48;
+        printf("%d\n",value );
+        if (line[0] == 'f') {
+            forward += value;
+        } else if (line[0] == 'u') {
+            depth -= value;
+        } else if (line[0] == 'd') {
+            depth += value;
+        }
+    }
+
+    fclose(fp);
+    printf("%d\n", forward);
+    printf("%d\n", depth);
+    printf("%d\n", forward*depth);
+
+    return 0;
+}
