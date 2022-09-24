1f6f7e94a50b0adc181f4a97181161b780de9582
AlexPegg <alexpeggsailing@gmail.com>
2022-09-24 19:35:45 +0000

    bump

--- Day 3.1: Binary Diagnostic.py
+++ Day 3.1: Binary Diagnostic.py
@@ -8,32 +8,7 @@ lines = f.readlines()
 
 newLines = []
 
-for column in range(0, len(lines[0]) - 3):
-    common = 0
-    for line in lines:
-        if line[column] == '1':
-            common += 1
-        else:
-            common -= 1
-    if common > 0 :
-        most = 1
-    else:
-        most = 0
-    
-    #remove numbers that don't start with that
-    for i in range(0, len(lines) - 1):
-        if int(lines[i][column]) == most:
-            newLines.append(lines[i])
-    lines = newLines
-    newLines = []
-    print(lines)
-oxygenBin = lines[0]
-
-f.close()
-f = open("day3.txt","r")
-lines = f.readlines()
-
-for column in range(0, len(lines[0]) - 3):
+for column in range(0, len(lines[0])-1):
     common = 0
     for line in lines:
         if line[column] == '1':
@@ -41,31 +16,60 @@ for column in range(0, len(lines[0]) - 3):
         else:
             common -= 1
     if common >= 0 :
-        least = 0
+        most = 1
     else:
-        least = 1
+        most = 0
     
     #remove numbers that don't start with that
-    for i in range(0, len(lines) - 1):
-        if int(lines[i][column]) == least:
+    for i in range(0, len(lines)):
+        if int(lines[i][column]) == most:
             newLines.append(lines[i])
     lines = newLines
     newLines = []
+    
+oxygenBin = lines[0]
+
+f.close()
+f = open("day3.txt","r")
+lines = f.readlines()
+
+for column in range(0, len(lines[0])-1):
+    common = 0
+    for line in lines:
+        print(line[column])
+        if line[column] == '1':
+            common += 1
+        else:
+            common -= 1
+    if common > 0 : #number 1 is more common
+        most = 1
+    else:            # even or 0 is more common
+        most = 0
+    
+    #remove numbers that don't start with that
+    for i in range(0, len(lines)):
+        if int(lines[i][column]) != most:
+            newLines.append(lines[i])
+    lines = newLines
+    newLines = []
+    print(lines)
     if len(lines) == 1:
         break
+    
 carbonBin = lines[0]
 
 f.close()
 
-
+print(oxygenBin)
+print(carbonBin)
 
 oxygen = 0
 carbon = 0
-for i in range(0, len(oxygenBin) -3):
+for i in range(0, len(oxygenBin) -1):
     if int(oxygenBin[i]) > 0:
         oxygen |= 2 ** (len(line)-(i)-2)
     if int(carbonBin[i]) > 0:
         carbon |= 2 ** (len(line)-(i)-2)
 print(oxygen)
 print(carbon)
-print(oxygen * carbon);
+print(oxygen * carbon)
