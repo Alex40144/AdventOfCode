354d847eb74e7a895c9e73c0b713aaa81f776b42
AlexPegg <alexpeggsailing@gmail.com>
2022-12-07 07:14:31 +0000

    day 6

---
+++ 2022/Day 5_2_Supply Stacks.py
@@ -0,0 +1,51 @@
+f = open("2022\inputs\day5.txt","r")
+lines = f.readlines()
+
+state = 0
+stack = []
+for line in lines:
+    line = line.strip("\n")
+    if (line.startswith(" 1")):
+        state = 1
+
+        continue
+    if(line == ""):
+        # rotate list
+        stack = list(zip(*stack[::-1]))
+        stack = [list(i) for i in stack]
+        print(stack)
+        continue
+    #load crates into array
+    if (state == 0):
+     
+        stack.append([line[i] for i in range(1, len(line), 4)])
+        print(stack)
+    if (state == 1):
+        line = line.split(' ')
+        mo = int(line[1])
+        fr = int(line[3]) - 1
+        to = int(line[5]) - 1
+        print(line)
+        for i in range(0, mo):
+            fromIndex   = ''.join(stack[fr]).rindex(next(s for s in reversed(stack[fr]) if (s != ' '))) - mo + 1 + i
+            try:
+                toIndex = stack[to].index(next(s for s in stack[to] if (s == ' ')))
+            except:
+                stack[to].append(' ')
+                toIndex = len(stack[to]) - 1
+
+            print(str(fromIndex) + " " + str(toIndex))
+            stack[to][toIndex] = stack[fr][fromIndex]
+            stack[fr][fromIndex] = ' '
+
+            print(stack)
+
+    
+top = ''
+for col in stack:
+    top += col[col.index(next(s for s in reversed(col) if s != ' '))]
+
+print(top)
+
+#list of guesses
+#DBGHDJDGG
---
+++ 2022/Day 6_2_Tuning Trouble.py
@@ -0,0 +1,7 @@
+f = open("2022\inputs\day6.txt","r")
+lines = f.readlines()
+line = list(lines[0])
+
+number = next(i for i in range(len(line)-1) if len(line[i: i + 14]) == len(set(line[i: i + 14])))
+
+print(number + 14)
